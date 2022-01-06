f8c4ece6f4c2582d6c476496198ba4ec8b57d075
Sharanya Sable <84729023+shharyaaa@users.noreply.github.com>
2022-01-06 22:26:28 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE studentData(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Sharanya', 20111024, 'Amravati',70579896, 'ABC12345');
+INSERT INTO studentData values('Manish', 20111025, 'Solapur', 79797979, 'PQR12345');
+INSERT INTO studentData VALUES('Shivam', 20111034, 'Daund', 21212121, 'ZXC12345');
+INSERT INTO studentData values('Snehal', 20111014, 'Pune', 84289834, 'BQR12345');
+INSERT INTO studentData values('Pratik', 20111023, 'Ahmednagar', 12567890, 'BVC12345');
+
